10c49fbb29eefc1942e936d8244295b2cf59ae48
Mukul Ram <mukul.ram97@gmail.com>
2017-04-24 17:06:51 -0400

    Updated error message

--- open_site.py
+++ open_site.py
@@ -38,4 +38,4 @@ if (num_args >= 3):
 # Error message.
 else:
     print("\nPlease use format:\npython open_browser_website.py [browser] [website1]" +
-    "[website2]...\nOr if you have alias \"site\" set up:\n site [browser] [website1] [website2]...\n\n");
+    "[website2]...\n\nOr if you have alias \"site\" set up:\nsite [browser] [website1] [website2]...\n\n");
